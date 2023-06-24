49c7478b88d8dcbc9ad626aa1fc10b0a0a4cbfee
Ye Fang "Norman" Kuang <normankuang@msn.com>
2023-06-24 00:45:59 -0400

    question functionality

--- src/components/pages/question/index.jsx
+++ src/components/pages/question/index.jsx
@@ -1,21 +1,48 @@
 import React from 'react'
+import './question.css'
 import PropTypes from 'prop-types'
 import { useParams,
         Link } from 'react-router-dom';
-import { Button } from 'react-bootstrap';
+import { Container, Row, Button } from 'react-bootstrap';
 import { Questions } from '../../../questions';
 import { Form } from 'react-bootstrap';
 
 export const Question = (props) => {
     const { id } = useParams();
+    let hint = false;
+
+    const displayHint = () => {
+        hint = !hint
+
+        if (hint == false) {
+            document.getElementById('hint').style.visibility = 'hidden'
+        } else {
+            document.getElementById('hint').style.visibility = 'visible'
+        }
+    }
+
+    const validateAnswer = (e) => {
+        if (!Questions[id].answers.some(answer => e.target.value.toUpperCase()  === answer.toUpperCase() )) {
+            e.target.style.background = '#FFCCCB'
+        } else {
+            e.target.style.background = '#DFFFE9'
+            e.target.disabled = true
+            document.getElementById('hintButton').disabled = true
+        }
+    }
 
     return (
-        <>
+        <Container className='question-container'>
             <p>{ Questions[id].question }</p>
-            <Form.Control size="lg" type="text" placeholder="Answer" />
-            <Button variant="warning">Hint</Button><br />
-            <Link to="/"><Button>Go back</Button></Link>
-        </>
+            <Form.Control size="lg" type="text" placeholder="Answer" onChange={validateAnswer} />
+            <div>
+                <Button variant="warning" id="hintButton" onClick={displayHint}>Hint</Button><br />
+                <p id="hint">{ Questions[id].hint }</p>
+            </div>
+            <div>
+                <Link to="/"><Button>Go back</Button></Link>
+            </div>
+        </Container>
     )
 }
 
